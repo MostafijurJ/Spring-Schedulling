1ecff2ecc476e2212fc17b2feb3b450a7a4c0bc8
Mostafijur <mrkajol550@gmail.com>
2022-06-04 09:59:32 +0600

    sketch ready

---
+++ src/main/java/com/learn/spring/schedulling/SpringSchedullingApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring.schedulling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedullingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchedullingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/spring/schedulling/SpringSchedullingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.spring.schedulling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedullingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
